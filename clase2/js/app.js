6069687ddac2ac76813ea479e92b99dd9e39294c
getsai18 <20243ds167@utez.edu.mx>
2025-10-06 13:49:17 -0600

    codigo

---
+++ clase2/js/app.js
@@ -0,0 +1,6 @@
+ var nombre;
+        let num1 = parseInt(docum);
+        let num2;
+        let mostrarResultado = document.getElementById("resultado");
+        resultado = num1 + num2;
+        mostrarResultado.innerHTML = "<p> el resulatdo es: "+resultado+"</p>";
